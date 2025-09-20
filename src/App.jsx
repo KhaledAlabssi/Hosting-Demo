2e1b4462c46bd3d68f72b8259f40d5668f22bb30
KhaledAlabssi <khaled.absi5@gmail.com>
2025-09-20 12:14:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import { Link, Route, Routes } from 'react-router'
+import About from './pages/About'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Link to={"/"} className='btn btn-primary'>Home</Link>
+    <Link to={"/about"} className='ml-2 btn btn-primary'>About</Link>
+
+    <Routes>
+
+      <Route path='/' element={<> 
+       <div className="carousel w-full">
+  <div id="item1" className="carousel-item w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+      className="w-full" />
+  </div>
+  <div id="item2" className="carousel-item w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+      className="w-full" />
+  </div>
+  <div id="item3" className="carousel-item w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+      className="w-full" />
+  </div>
+  <div id="item4" className="carousel-item w-full">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+      className="w-full" />
+  </div>
+</div>
+<div className="flex w-full justify-center gap-2 py-2">
+  <a href="#item1" className="btn btn-xs">1</a>
+  <a href="#item2" className="btn btn-xs">2</a>
+  <a href="#item3" className="btn btn-xs">3</a>
+  <a href="#item4" className="btn btn-xs">4</a>
+</div></>
+
+
+      } />
+      <Route path='/about' element={<About />} />
+    </Routes>
+
+
+     
+    </>
+  )
+}
+
+export default App
